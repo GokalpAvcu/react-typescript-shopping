29d35bd07ffbe1bf78a68be557a85d8b1751000a
Gökalp Avcu <gokalp.avcu@santsg.com>
2022-09-12 16:03:19 +0300

    add changes

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Button, Card } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 import { formatCurrency } from "../utilities/formatCurrency";
 
 type StoreItemProps = {
@@ -9,7 +10,13 @@ type StoreItemProps = {
 };
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
-  const quantity = 0;
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShoppingCart()
+  const quantity = getItemQuantity(id)
   return (
     <Card className="h-100">
       <Card.Img variant="top" src={imgUrl} height="250" />
@@ -23,26 +30,28 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         </Card.Title>
         <div className="mt-auto">
           {quantity === 0 ? (
-            <Button className="w-100">+ Add to Cart</Button>
-          ) : ( <div className="d-flex align-items-center flex-column"
-              style={{ gap: ".5rem" }}>
-                <div className="d-flex align-items-center justify-content-center"
-                style={{ gap: ".5rem" }}>
-               <Button>-</Button>
-               <div>
-               <span className="fs-3"> {quantity} </span> 
-               in cart
-               </div> 
-               <Button>+</Button>
-               </div>
-               <Button variant="danger" size="sm">
-                Remove 
-                </Button>
-
-               
-              {" "}
+            <Button className="w-100" onClick={() => increaseCartQuantity(id)}>+ Add to Cart</Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{ gap: ".5rem" }}
+            >
+              <div
+                className="d-flex align-items-center justify-content-center"
+                style={{ gap: ".5rem" }}
+              >
+                <Button>-</Button>
+                <div>
+                  <span className="fs-3"> {quantity} </span>
+                  in cart
+                </div>
+                <Button>+</Button>
+              </div>
+              <Button variant="danger" size="sm">
+                Remove
+              </Button>{" "}
             </div>
-          )} 
+          )}
         </div>
       </Card.Body>
     </Card>
