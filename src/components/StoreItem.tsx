6eed37d1666f102113beeaf38dbcb26c50432040
Gökalp Avcu <gokalp.avcu@santsg.com>
2022-09-12 16:07:38 +0300

    add context

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -40,14 +40,18 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
                 className="d-flex align-items-center justify-content-center"
                 style={{ gap: ".5rem" }}
               >
-                <Button>-</Button>
+                <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
                 <div>
                   <span className="fs-3"> {quantity} </span>
                   in cart
                 </div>
-                <Button>+</Button>
+                <Button onClick={() => increaseCartQuantity(id)}>+</Button>
               </div>
-              <Button variant="danger" size="sm">
+              <Button 
+              onClick={() => removeFromCart(id)}
+              variant="danger" 
+              size="sm"
+              >
                 Remove
               </Button>{" "}
             </div>
